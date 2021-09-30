605725b97f4d4585f552a51e1af058b3d1ee0607
KJH-0105 <juhyun7955@naver.com>
2021-09-30 22:00:23 +0900

    카테고리 : 수학

---
+++ IntelliJAlgo/src/Programmers/SummerWinterCoding/멀쩡한사각형.java
@@ -0,0 +1,11 @@
+package Programmers.SummerWinterCoding;
+
+import java.math.BigInteger;
+
+public class 멀쩡한사각형 {
+
+    public long solution(int w, int h) {
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        return ((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd);
+    }
+}
