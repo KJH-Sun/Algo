9cfd4d62d078eb6625739e10ff216ae5d77bdc66
KJH-0105 <juhyun7955@naver.com>
2021-09-30 22:04:10 +0900

    카테고리 : DP 전형적인 DP 형태의 문제. 생각보다 난이도 있는 문제였다.

---
+++ IntelliJAlgo/src/Programmers/SummerWinterCoding/스티커모으기2.java
@@ -0,0 +1,24 @@
+package Programmers.SummerWinterCoding;
+
+public class 스티커모으기2 {
+
+    public static int solution(int sticker[]) {
+        int len = sticker.length;
+        if (len == 1) {
+            return sticker[0];
+        }
+        int[][] dp = new int[2][len];
+        dp[0][0] = sticker[0];
+        dp[0][1] = sticker[0];
+        dp[1][0] = 0;
+        dp[1][1] = sticker[1];
+        for (int i = 2; i < len; i++) {
+            dp[1][i] = Math.max(dp[1][i - 2] + sticker[i], dp[1][i - 1]);
+            if (i == len - 1) {
+                break;
+            }
+            dp[0][i] = Math.max(dp[0][i - 2] + sticker[i], dp[0][i - 1]);
+        }
+        return Math.max(dp[0][len - 2], dp[1][len - 1]);
+    }
+}
